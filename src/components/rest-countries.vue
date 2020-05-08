c05199225cfb7717c11ffe03625d80ee36daa5e5
WillemTaylor <willemtaylor@msn.com>
2020-05-08 12:28:33 +0100

    updated countries mobile image1

--- src/components/rest-countries.vue
+++ src/components/rest-countries.vue
@@ -5,7 +5,7 @@
       <img src="../assets/countries_mobile.png" class="portfolio-img desktop-hide-v2" />
     </div>
     <div class="text">
-      <a href="https://restful-countries.netlify.app/" target="_blank">Profiling Rick & Morty</a>
+      <a href="https://restful-countries.netlify.app/" target="_blank">RESTFUL Countries list</a>
       <br />
       <a href="https://github.com/WillemTaylor/rest-countries" target="_blank">Github</a>
     </div>
